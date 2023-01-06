644f1d65f103af24393ceea1b6028191f90f91ed
VladZGarcia <vlad.garcia@gmail.com>
2023-01-06 03:54:39 +0100

    better structure

--- shoppinglista/ContentView.swift
+++ shoppinglista/ContentView.swift
@@ -9,22 +9,50 @@ import SwiftUI
 import Firebase
 import FirebaseAuth
 
+// 1. förbättra strukturen genom att länna över shoppinglistan (items)
+// i ett oservable object
+//
+// 2. användaren kan välja att skapa konto i en inställnings sida.
+
 struct ContentView: View {
-    let db = Firestore.firestore()
-    
-    @ObservedObject var storeItems = StoreItems()
-    @State var newItem : String = ""
     @State var signedIn = false
     
-    init() {
-        Auth.auth().signInAnonymously { authResult, error in
-            if let error = error {
-                print("error signing in \(error)")
-            } else {
-                print("Signed in \(Auth.auth().currentUser?.uid)")
-            }
+    var body: some View {
+        if !signedIn {
+            SigningInView(signedIn: $signedIn)
+        } else {
+            ShoppingListView()
         }
     }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct SigningInView: View {
+    @Binding var signedIn : Bool
+    var body: some View {
+        Text("Signing In")
+            .onAppear() {
+                Auth.auth().signInAnonymously { authResult, error in
+                    if let error = error {
+                        print("error signing in \(error)")
+                    } else {
+                        print("Signed in \(Auth.auth().currentUser?.uid)")
+                        signedIn = true
+                    }
+                }
+            }
+    }
+}
+
+struct ShoppingListView: View {
+    let db = Firestore.firestore()
+    @State var newItem : String = ""
+    @ObservedObject var storeItems = StoreItems()
     
     var searchBar : some View {
         HStack {
@@ -42,21 +70,8 @@ struct ContentView: View {
                 List{
                     ForEach(storeItems.items) {
                         item in
-                        HStack {
-                            Text(item.itemName)
-                            Spacer()
-                            Button(action: {
-                                if let id = item.id,
-                                   let user = Auth.auth().currentUser
-                                {
-                                    db.collection("users").document(user.uid)
-                                        .collection("items").document(id).updateData(["isChecked": !item.isChecked])
-                                }
-                            }) {
-                                Image(systemName: item.isChecked ? "checkmark.square" : "square")
-                            }
-                        }
-                    }.onMove(perform: self.move)
+                        ItemRowView(item: item)
+                    }.onMove(perform: move)
                         .onDelete() { indexSet in
                             for index in indexSet {
                                 let item = storeItems.items[index]
@@ -71,16 +86,41 @@ struct ContentView: View {
                     listenToFirestore()
                 }
                 .navigationBarTitle("ShoppingList")
-                    .navigationBarItems(trailing: EditButton())
+                .navigationBarItems(trailing: EditButton())
             }
             .padding()
         }
     }
+        
+        func move(from source : IndexSet, to destination : Int) {
+            storeItems.items.move(fromOffsets: source, toOffset: destination)
+        }
+        
+        func listenToFirestore() {
+            guard let user = Auth.auth().currentUser else {return}
+            
+            db.collection("users").document(user.uid).collection("items").addSnapshotListener { snapshot, err in
+                guard let snapshot = snapshot else {return}
+                
+                if let err = err {
+                    print("Error getting document \(err)")
+                } else {
+                    storeItems.items.removeAll()
+                    for document in snapshot.documents {
+                        let result = Result{
+                            try document.data(as: ItemList.self)
+                        }
+                        switch result {
+                        case .success(let item) :
+                            storeItems.items.append(item)
+                        case .failure(let error) :
+                            print("Error decoding item: \(error)")
+                        }
+                    }
+                }
+            }
+        }
     
-    func addNewItem() {
-        saveToFirestore(newItem)
-        self.newItem = ""
-    }
     
     func saveToFirestore(_ newItem: String) {
         let item = ItemList(itemName: newItem)
@@ -92,42 +132,31 @@ struct ContentView: View {
             print("Error savin to DB")
         }
     }
-    func listenToFirestore() {
-        guard let user = Auth.auth().currentUser else {return}
-        
-        db.collection("users").document(user.uid).collection("items").addSnapshotListener { snapshot, err in
-            guard let snapshot = snapshot else {return}
-            
-            if let err = err {
-                print("Error getting document \(err)")
-            } else {
-                storeItems.items.removeAll()
-                for document in snapshot.documents {
-                    let result = Result{
-                        try document.data(as: ItemList.self)
-                    }
-                    switch result {
-                    case .success(let item) :
-                        storeItems.items.append(item)
-                    case .failure(let error) :
-                        print("Error decoding item: \(error)")
-                    }
-                }
-            }
-        }
-    }
     
-    func move(from source : IndexSet, to destination : Int) {
-        storeItems.items.move(fromOffsets: source, toOffset: destination)
-    }
-    
-    func delete(at offsets : IndexSet) {
-        storeItems.items.remove(atOffsets: offsets)
+    func addNewItem() {
+        saveToFirestore(newItem)
+        self.newItem = ""
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ItemRowView: View {
+    let db = Firestore.firestore()
+    let item : ItemList
+    
+    var body: some View {
+        HStack {
+            Text(item.itemName)
+            Spacer()
+            Button(action: {
+                if let id = item.id,
+                   let user = Auth.auth().currentUser
+                {
+                    db.collection("users").document(user.uid)
+                        .collection("items").document(id).updateData(["isChecked": !item.isChecked])
+                }
+            }) {
+                Image(systemName: item.isChecked ? "checkmark.square" : "square")
+            }
+        }
     }
 }
